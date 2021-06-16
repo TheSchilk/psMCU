32990750a6d21315445361c3ec3bf3a70878a596
TheSchilk <schilk.philipp@gmail.com>
2021-06-16 12:02:28 +0200

    psPROG WSL bridge + makefile 'flash' target

---
+++ psPROG/Utility/install_wsl.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo "Please ensure that psPROG.bat is found in the windows path!"
+sudo cp wsl_run.sh /usr/local/bin/psPROG
---
+++ psPROG/Utility/wsl_run.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+echo "psPROG.bat "$@ | cmd.exe
