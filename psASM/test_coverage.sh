ab42751bcb39ba477c8c3bcd5d6bab1f5c178be2
TheSchilk <schilk.philipp@gmail.com>
2021-11-13 11:58:29 +0100

    psASM: ignored test runner during coverage

--- psASM/test_coverage.sh
+++ psASM/test_coverage.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-coverage run --omit Parsing/psASMantlr4/psASMLexer.py,Parsing/psASMantlr4/psASMParser.py,Parsing/psASMantlr4/psASMParserVisitor.py test_psASM.py
+coverage run --omit test_psASM.py,Parsing/psASMantlr4/psASMLexer.py,Parsing/psASMantlr4/psASMParser.py,Parsing/psASMantlr4/psASMParserVisitor.py test_psASM.py
 coverage report
 coverage html
